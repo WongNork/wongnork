cc07ba26da6593dab07b0a23c1110f8fceb60c0b
Icezu <krasin.d@ku.th>
2021-10-24 00:03:18 +0700

    Create a login page

--- wongnork/urls.py
+++ wongnork/urls.py
@@ -5,5 +5,6 @@ app_name = "wongnork"
 
 
 urlpatterns = [
-    path("register/", views.register_request, name="register")
+    path("register/", views.register_request, name="register"),
+    path("login/", views.login_request, name="login")
 ]
--- wongnork/views.py
+++ wongnork/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import NewUserForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
@@ -13,8 +14,27 @@ def register_request(request):
             user = form.save()
             login(request, user)
             messages.success(request, "Registration successful.")
-            return redirect("wongnork:homepage")
+            return redirect("wongnork:register")
         messages.error(
             request, "Unsuccessful registration. Invalid information.")
     form = NewUserForm()
     return render(request, "register.html", {"register_form": form})
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("wongnork:register")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request, "login.html", {"login_form": form})
